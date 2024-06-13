c07299fce5285b725e186da83343f1953e6aecd7
bbotelho <brunnowr.bw@gmail.com>
2024-06-13 22:17:37 +0200

    automatic save home

---
+++ oddEvenNumbers/OddEven.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class OddEven{
+ public static void main(String[] args){
+   System.out.print("Introduzca un numero: ");
+   Scanner scan = new Scanner(System.in);
+   boolean check = false;
+   int num = Integer.parseInt(scan.nextLine());
+   isOddEven(num);
+   scan.close();
+  } 
+
+ public static void isOddEven(int num){
+   if (num % 2 == 0)
+      System.out.println("El numero "+num+" es par!");
+   else
+      System.out.println("El numero "+num+" es impar!");
+ }
+}
